6b5a181c7a94ccc2d683680467a0b983932a50e9
Alexandr <alexandr.dyomin@yahoo.com>
2023-10-31 15:57:58 +0900

    переписал throttle, debounce, добавил createHtmlElements

--- utils.js
+++ utils.js
@@ -19,36 +19,63 @@ function debounce(fn, delay, immediately = false) {
 	let delayedCallTimer = null;
 
 	return function debounced(...args) {
+		const callFnNow = (args) => {
+			immediateCallTimer = setTimeout(clearImmediateCallTimer, delay);
+			fn.apply(this, args);
+		}
+		
+		const callFnLater = (args) => {
+			delayedCallTimer = setTimeout(() => {
+				fn.apply(this, args);
+				immediately && (immediateCallTimer = setTimeout(clearImmediateCallTimer, delay));
+				delayedCallTimer = null;
+			}, delay);
+		}
+
+		function clearImmediateCallTimer() {
+			immediateCallTimer = null;
+		}	
+
 		if (immediately && !immediateCallTimer) return callFnNow(args);
 		
 		immediately && clearTimeout(immediateCallTimer);
 		delayedCallTimer && clearTimeout(delayedCallTimer);
 		callFnLater(args);
 	}	
-
-	function callFnNow(args) {
-		immediateCallTimer = setTimeout(clearImmediateCallTimer, delay);
-		fn.apply(this, args);
-	}
-
-	function callFnLater(args) {
-		delayedCallTimer = setTimeout(function callFn() {
-			fn.apply(this, args);
-			immediately && (immediateCallTimer = setTimeout(clearImmediateCallTimer, delay));
-			delayedCallTimer = null;
-		}, delay);
-	}
-
-	function clearImmediateCallTimer() {
-		immediateCallTimer = null;
-	}	
 }
 
+function throttle(func, ms) {
+	let timer = null;
+
+    function wrapper(...args) {
+        if (timer) {
+            return;
+        }
+
+        func.apply(this, args);
+
+        timer = setTimeout(() => {
+			wrapper.apply(this, args);
+			clearTimeout(timer);
+			timer = null;
+        }, ms);
+    }
+
+    return wrapper;
+}
+
+function createHtmllements(descriptions) {
+    let elements = [];
+    for (let i = 0; i < descriptions.length; i +=2) {
+        elements.push(createHtmlElement(descriptions[i], descriptions[i+1]));
+    }
+    return elements;
+}
 
 function createHtmlElement(tagName, props = {}) {
     let htmlElement = document.createElement(tagName);
     for (let prop in props) {
-        htmlElement[prop] = props[prop];
+		htmlElement.setAttribute(prop, props[prop]);
     };
     return htmlElement;
 }
@@ -63,4 +90,4 @@ function mergeIntoComponent(root, elements) {
     return root;
 }
 
-export { curry, promisify, debounce, createHtmlElement, mergeIntoComponent };
+export { curry, promisify, debounce, throttle, createHtmllements, createHtmlElement, mergeIntoComponent };
